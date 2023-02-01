a5844e48855a4d22dc052a5058c61db3ee4bd901
Italo-Alan <italoalan123@gmail.com>
2023-02-01 16:11:22 -0300

    :tada: Commit inicial

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.css']
+})
+export class ExemplosPipesComponent {
+
+}
